943877946349a2493304250c2ccec6ea21e44dbd
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-07 19:09:42 +0800

    Add function to display repo data in html file

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,18 +1,38 @@
 function userInformationHTML(user) {
   return `
-  <h2>${user.name}
-    <span class="small-name">
-      (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
-    </span>
-  </h2>
-  <div class="gh-content">
-  <div class="gh-avatar">
-  <a href="${user.html_url}" target="_blank">
-  <img src="${user.avatar_url}" width="80px" height="80" alt="${user.login}" />  
-  </a>
-  </div>
-  <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-  </div>`;
+      <h2>${user.name}
+        <span class="small-name">
+          (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+      </h2>
+      <div class="gh-content">
+        <div class="gh-avatar">
+          <a href="${user.html_url}" target="_blank">
+            <img src="${user.avatar_url}" width="80px" height="80" alt="${user.login}" />  
+          </a>
+        </div>
+        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+      </div>`;
+}
+
+function repoInformationHTML(repos) {
+  if (repos.length == 0) {
+    return `<div class="clearfix repo-list">No repos!</div>`
+  }
+
+  var listItemsHTML = repos.map(function (repo) {
+    return `<li>
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </li>`;
+  });
+  return `<div class="clearfix repo-list">
+            <p>
+              <strong>Repo List:</strong>
+            </p>
+            <ul>
+              ${listItemsHTML.join("\n")}
+            </ul>
+          </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -29,13 +49,13 @@ function fetchGitHubInformation(event) {
       </div>`);
   $.when(
     $.getJSON(`https://api.github.com/users/${username}`),
-    $.getJSON(`https://api.github.com/users/${username}/repos`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
   ).then(
     function (firstResponse, secondResponse) {
       var userData = firstResponse[0];
       var repoData = secondResponse[0];
       $("#gh-user-data").html(userInformationHTML(userData));
-      $('gh-repo-data').html(repoInformationHTML(repoData));
+      $("#gh-repo-data").html(repoInformationHTML(repoData));
     },
     function (errorResponse) {
       if (errorResponse.status === 404) {
